bc0114b8cc661721c8f3786145af9a8a42c6abd8
Jae Perris <jae.perris@gmail.com>
2020-03-28 18:47:46 +0100

    Remove redundant headers from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,3 @@
-import Head from 'next/head';
-import { HeaderBar } from '../components/header-bar';
-
-const Home = (): React.ReactElement => (
-  <div className="container">
-    <Head>
-      <title>TMF Design Portfolio</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main>
-      <HeaderBar></HeaderBar>
-    </main>
-  </div>
-);
+const Home = (): React.ReactElement => <div className="container">THIS IS A HOME PAGE</div>;
 
 export default Home;
