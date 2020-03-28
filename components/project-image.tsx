6d3f33949bcee8fe3519632bbe7e5e8068da48e9
Jae Perris <jae.perris@gmail.com>
2020-03-28 19:38:45 +0100

    Add ProjectImages

--- components/project-image.tsx
+++ components/project-image.tsx
@@ -1,12 +1,30 @@
-export type ProjectImageProps = {};
+import css from 'styled-jsx/css';
 
-export const ProjectImage = (): React.ReactElement<ProjectImageProps> => {
-  return <div className="container">{style}</div>;
+export type ProjectImageProps = {
+  images: { src: string; alt?: string }[];
 };
 
-const style = (
-  <style jsx>{`
-    .container {
-    }
-  `}</style>
+export const ProjectImages = ({
+  images,
+}: ProjectImageProps): React.ReactElement<ProjectImageProps> => {
+  return (
+    <div className="container">
+      {createImages(images, style)}
+      <style jsx>{style}</style>
+    </div>
+  );
+};
+
+const style = css`
+  .container {
+  }
+`;
+
+const createImages = (images: ProjectImageProps['images'], style?: string): React.ReactElement => (
+  <>
+    {images.map((image, index) => (
+      <img key={index} src={image.src} alt={image.alt} />
+    ))}
+    <style jsx>{style}</style>
+  </>
 );
