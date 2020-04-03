e974c1cb7648609c3138ac3fb643a886860172cb
Jae Perris <jae.perris@gmail.com>
2020-04-03 16:08:31 +0200

    Cleanup header-bar

--- components/header-bar.tsx
+++ components/header-bar.tsx
@@ -1,25 +1,51 @@
 import css from 'styled-jsx/css';
-import { makeList } from '../utility/makeList';
 
 export type HeaderBarProps = typeof defaultHeaderBarProps;
 
 const defaultHeaderBarProps = {
   leftLinks: [
-    { href: '/', text: 'Home' },
-    { href: '/projects', text: 'Work' },
+    { href: '/', text: 'home' },
+    { href: '/work', text: 'work' },
   ],
-  rightLinks: [{ href: '/about', text: 'About' }],
+  rightLinks: [{ href: '/about', text: 'about' }],
+  pathname: '',
 };
 
-export const HeaderBar = ({ leftLinks, rightLinks }: HeaderBarProps): React.ReactElement => {
+export const HeaderBar = ({
+  leftLinks,
+  rightLinks,
+  pathname,
+}: HeaderBarProps): React.ReactElement => {
   return (
     <div className="container">
-      <div className="left-links">{makeList(leftLinks, style)}</div>
-      <div className="right-links">{makeList(rightLinks, style)}</div>
+      <div className="left-links">{makeList(leftLinks, pathname, style)}</div>
+      <div className="right-links">{makeList(rightLinks, pathname, style)}</div>
       <style jsx>{style}</style>
     </div>
   );
 };
+
+const makeList = (
+  links: { href: string; text: string }[],
+  pathname: string,
+  style?: string,
+): React.ReactElement => (
+  <ul>
+    {links.map((link, index) => {
+      const currentPage = pathname.split('/')[1] === link.href.replace('/', '');
+      return (
+        <li key={index}>
+          <h2 className={currentPage ? 'current-page' : ''}>
+            <a className={currentPage ? 'current-page' : ''} href={link.href}>
+              {link.text}
+            </a>
+          </h2>
+        </li>
+      );
+    })}
+    <style jsx>{style}</style>
+  </ul>
+);
 
 HeaderBar.defaultProps = defaultHeaderBarProps;
 
@@ -28,6 +54,11 @@ const style = css`
     width: 100%;
     display: flex;
     justify-content: space-between;
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+  h2 {
+    display: inline;
   }
   ul {
     margin: 0;
@@ -38,37 +69,40 @@ const style = css`
     margin: 0;
     padding: 0;
   }
-  a {
-    margin-left: 10px;
-    margin-right: 10px;
-  }
 
   .left-links {
     display: flex;
     justify-content: flex-start;
   }
+
+  .left-links a {
+    margin-left: 0;
+    margin-right: 20px;
+  }
+
+  .right-links a {
+    margin-left: 20px;
+    margin-right: 0;
+  }
   .right-links {
     display: flex;
     justify-content: flex-end;
   }
+  a {
+    text-decoration: none;
+    color: orange;
+  }
+
+  .current-page {
+    color: black;
+  }
+
+  a:hover,
+  a:hover::selection {
+    color: pink;
+  }
+
+  a::selection {
+    color: hotpink;
+  }
 `;
-
-export const withHeader = <T extends HeaderBarProps>(
-  WrappedComponent: React.ComponentType<T>,
-): React.FC<T & HeaderBarProps> => {
-  const displayName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
-  const component = ({
-    leftLinks,
-    rightLinks,
-    ...props
-  }: T & HeaderBarProps): React.ReactElement => (
-    <>
-      <HeaderBar leftLinks={leftLinks} rightLinks={rightLinks}></HeaderBar>
-      <WrappedComponent {...(props as T)}></WrappedComponent>
-    </>
-  );
-
-  component.displayName = displayName;
-
-  return component;
-};
