03520fb8c945316a5f75dcb724310a8e7c68e8d5
Jae Perris <jae.perris@gmail.com>
2020-04-03 13:03:57 +0200

    Create project page style

--- pages/projects/alacrity.tsx
+++ pages/projects/alacrity.tsx
@@ -1,5 +1,5 @@
-import css from 'styled-jsx/css';
 import { SimpleSlider } from '../../components/simple-slider';
+import { style } from '../../components/project-page-style';
 
 const Alacrity = (): React.ReactElement => {
   return (
@@ -43,54 +43,5 @@ const Alacrity = (): React.ReactElement => {
     </div>
   );
 };
-
-const style = css`
-  .container {
-    padding-left: 4%;
-    padding-right: 4%;
-    width: 100%;
-    color: black;
-    font-family: sans-serif;
-    font-size: 20px;
-    text-align: justify;
-  }
-
-  p {
-    margin-bottom: 1em;
-  }
-
-  ::selection {
-    background: inherit;
-    color: turquoise;
-  }
-
-  h2 {
-    font-style: bold;
-  }
-
-  .date,
-  .exhibited {
-    font-size: 22px;
-    margin-bottom: 1em;
-  }
-
-  .exhibited {
-    font-style: italic;
-  }
-
-  a {
-    text-decoration: none;
-    color: orange;
-  }
-
-  a:hover,
-  a:hover::selection {
-    color: pink;
-  }
-
-  a::selection {
-    color: hotpink;
-  }
-`;
 
 export default Alacrity;
