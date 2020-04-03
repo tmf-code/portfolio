e953c28d230649f7de823553c23e379019296998
abaran3 <alexik11@hotmail.com>
2020-04-03 20:47:14 +0200

    Sort work page by years

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,23 +1,24 @@
+/* eslint-disable react/prop-types */
 import { HeaderBarProps } from '../../components/header-bar';
 
 export type ProjectIndexProps = typeof defaultProjectIndexProps & HeaderBarProps;
 
 const defaultProjectIndexProps = {
   projects: [
-    { href: 'work/alacrity', text: 'alacrity' },
-    { href: 'work/ambient-matter', text: 'ambient matter' },
-    { href: 'work/beep-text', text: 'beep text' },
-    { href: 'work/coast', text: 'the coast was clear' },
-    { href: 'work/feedself', text: 'feedself' },
-    { href: 'work/gan', text: 'generative adversarial networks' },
-    { href: 'work/interpassivity', text: 'beyond interpassivity' },
-    { href: 'work/ordinary-rambling', text: 'ordinary rambling' },
-    { href: 'work/textures', text: 'temporal textures' },
+    { href: 'work/alacrity', text: 'alacrity', year: '2019' },
+    { href: 'work/ambient-matter', text: 'ambient matter', year: '2019' },
+    { href: 'work/beep-text', text: 'beep text', year: '2019' },
+    { href: 'work/coast', text: 'the coast was clear', year: '2019' },
+    { href: 'work/feedself', text: 'feedself', year: '2018' },
+    { href: 'work/gan', text: 'generative adversarial networks', year: '2019' },
+    { href: 'work/interpassivity', text: 'beyond interpassivity', year: '2018' },
+    { href: 'work/ordinary-rambling', text: 'ordinary rambling', year: '2018' },
+    { href: 'work/textures', text: 'temporal textures', year: '2019' },
   ],
 };
 
 const ProjectIndex = ({ projects }: ProjectIndexProps): React.ReactElement => {
-  const list = (
+  const list = (projects: ProjectIndexProps['projects']) => (
     <ul>
       {projects.map((project, index) => (
         <li key={index}>
@@ -26,10 +27,19 @@ const ProjectIndex = ({ projects }: ProjectIndexProps): React.ReactElement => {
       ))}
     </ul>
   );
+  const years = projects.map(project => project.year);
+  const uniqueYears = Array.from(new Set(years)).sort().reverse();
+
   return (
     <div className="container">
-      <h2>2019</h2>
-      {list}
+      {uniqueYears.map((selectedYear, index) => {
+        return (
+          <div key={index}>
+            <h2> {selectedYear} </h2>
+            {list(projects.filter(project => project.year === selectedYear))}
+          </div>
+        );
+      })}
     </div>
   );
 };
