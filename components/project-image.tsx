14d698408be777df3d6b898b5aa16768194445b0
Jae Perris <jae.perris@gmail.com>
2020-04-04 19:14:25 +0200

    Add project image component

--- components/project-image.tsx
+++ components/project-image.tsx
@@ -1,16 +1,37 @@
-export type ProjectImageProps = {
-  images: { src: string; alt?: string }[];
+import styles from '../styles/project-image.module.scss';
+
+type ProjectImageProps = {
+  height?: React.CSSProperties['height'];
+  width?: React.CSSProperties['width'];
+  backgroundFit?: 'cover' | 'contain';
+  center?: boolean;
+  src: string;
 };
 
-export const ProjectImages = ({
-  images,
-}: ProjectImageProps): React.ReactElement<ProjectImageProps> => {
-  return <div className="container">{createImages(images)}</div>;
+export const ProjectImage = ({
+  src,
+  height = '400px',
+  width = '400px',
+  backgroundFit,
+  center = false,
+}: ProjectImageProps) => {
+  const containerWidth = center ? '100%' : width;
+  const imageWidth = center ? width : '100%';
+
+  return (
+    <div
+      style={{ width: containerWidth, height: height }}
+      className={styles['project-image-container']}
+    >
+      <div
+        style={{
+          backgroundImage: `url(${src})`,
+          height: '100%',
+          width: imageWidth,
+          backgroundSize: backgroundFit,
+        }}
+        className={styles['project-image']}
+      ></div>
+    </div>
+  );
 };
-const createImages = (images: ProjectImageProps['images']): React.ReactElement => (
-  <>
-    {images.map((image, index) => (
-      <img key={index} src={image.src} alt={image.alt} />
-    ))}
-  </>
-);
