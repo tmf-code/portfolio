3de1ce8bb4fc8346b994be1d88fa2dcd41ca128a
abaran3 <alexik11@hotmail.com>
2020-08-25 23:25:19 +0200

    Update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,8 +9,9 @@ const About = (): React.ReactElement => {
           <a href="/">Alexandra Barancová</a> and{' '}
           <a href="https://honga1.github.io/personal-portfolio/">Jae Perris</a>. Their work explores
           meanings of value in today’s technoscapes with a particular interest in nonhuman
-          interlocutors. Their main research themes include narratives of the Anthropocene, concepts
-          of Self, artificial intelligence(s), and machinic aesthetics.
+          interlocutors. Their main research themes include embodiment in video games, the influence
+          of digital media on notions of subjectivity, contemporary discourses around artificial
+          intelligence, and machinic aesthetics.
         </p>
         <p>
           Their works have taken the form of video essays, audiovisual installations, art games, and
